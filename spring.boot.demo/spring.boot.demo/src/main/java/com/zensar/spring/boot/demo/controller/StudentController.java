3f220d51b115fea41968258e27c2499949d4c6bb
puna1234 <105356038+puna1234@users.noreply.github.com>
2022-06-17 22:31:11 +0530

    Add files via upload
    Student

---
+++ spring.boot.demo/spring.boot.demo/src/main/java/com/zensar/spring/boot/demo/controller/StudentController.java
@@ -0,0 +1,79 @@
+package com.zensar.spring.boot.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zensar.spring.boot.DTO.*;
+import com.zensar.spring.boot.demo.entity.*;
+import com.zensar.spring.boot.demo.services.*;
+
+@RestController
+@RequestMapping(value = "/student-api", produces = { MediaType.APPLICATION_JSON_VALUE,
+		MediaType.APPLICATION_XML_VALUE }, consumes = { MediaType.APPLICATION_JSON_VALUE,
+				MediaType.APPLICATION_XML_VALUE })
+public class StudentController {
+
+	@Autowired
+	private StudentService studentService;
+
+	// @RequestMapping(value = "/students/{studentId}",method=RequestMethod.GET)
+	@GetMapping(value = "/students/{studentId}")
+	public ResponseEntity<StudentDto> getStudent(@PathVariable("studentId") int studentId) {
+		return new ResponseEntity<StudentDto>(studentService.getStudent(studentId), HttpStatus.OK);
+	}
+
+	// @RequestMapping(value = { "/students", "/listOfStudents"
+	// },method=RequestMethod.GET)
+	@GetMapping(value = { "/students", "/listOfStudents" })
+	public ResponseEntity<List<StudentDto>> getAllStudents(@RequestParam(value="pageNumber",required=false, defaultValue = "0")int pageNumber, @RequestParam(value="pageSize",required=false, defaultValue = "5")int pageSize) {
+		 
+		 return new ResponseEntity<List<StudentDto>>(studentService.getAllStudents(pageNumber,pageSize), HttpStatus.OK);
+	}
+
+	// @RequestMapping(value = "/students",method=RequestMethod.POST)
+	@PostMapping(value = "/students")
+	public ResponseEntity<StudentDto> insertStudent(@RequestBody StudentDto studentDto) {
+
+		return new ResponseEntity<StudentDto>(studentService.insertStudent(studentDto), HttpStatus.CREATED);
+
+	}
+
+	// @RequestMapping(value="/students/{studentId}",method=RequestMethod.PUT)
+	@PutMapping(value = "/students/{studentId}")
+	public ResponseEntity<String>updateStudent(@PathVariable("studentId") int studentId, @RequestBody StudentDto studentDto) {
+		studentService.updateStudent(studentId, studentDto);
+		return new ResponseEntity<String>("Student Updated Successfully..", HttpStatus.OK);
+
+	}
+
+	// @RequestMapping(value="/students/{studentId}",method=RequestMethod.DELETE)
+	@DeleteMapping("/students/{studentId}")
+	public ResponseEntity<String> deleteStudent(@PathVariable("studentId") int studentId) {
+		studentService.deleteStudent(studentId);
+		return new ResponseEntity<String>("Student Deleted Successfully..", HttpStatus.OK);
+	}
+	@GetMapping("/students/studentName/{studentName}")
+	public ResponseEntity<List<StudentDto>> getByStudentName(@PathVariable("studentName")String studentName){
+		return new ResponseEntity<List<StudentDto>>(studentService.getByStudentName(studentName), HttpStatus.OK);
+		
+	}
+	@GetMapping("/students/{studentName}/{studentAge}")
+public ResponseEntity<List<StudentDto>> findByStudentNameAndStudentAge(@PathVariable("studentName")String studentName,@PathVariable("studentAge") int age) {
+		
+		return new ResponseEntity<List<StudentDto>>(studentService.findByStudentNameAndStudentAge(studentName, age), HttpStatus.OK);
+	}
+
+}
