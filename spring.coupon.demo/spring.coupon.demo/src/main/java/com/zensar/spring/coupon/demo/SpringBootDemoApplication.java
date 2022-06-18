361891ad9ec9a284c89e5bc260bbfa0e318e46aa
puna1234 <105356038+puna1234@users.noreply.github.com>
2022-06-18 13:17:04 +0530

    updated Coupon Service

---
+++ spring.coupon.demo/spring.coupon.demo/src/main/java/com/zensar/spring/coupon/demo/SpringBootDemoApplication.java
@@ -0,0 +1,20 @@
+package com.zensar.spring.coupon.demo;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
+
+}
