66cb4ce50b9eeb8d4cd52aa26151069160c64ed1
puna1234 <105356038+puna1234@users.noreply.github.com>
2022-06-23 09:50:08 +0530

    student updated

---
+++ spring.boot.demo/src/main/java/com/zensar/spring/boot/dto/StudentDto.java
@@ -0,0 +1,54 @@
+package com.zensar.spring.boot.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class StudentDto {
+@Id
+	private int studentId;
+	private String studentName;
+	private int studentAge;
+
+	public StudentDto() {
+		super();
+	}
+
+	public StudentDto(int studentId, String studentName, int studentAge) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.studentAge = studentAge;
+	}
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public int getStudentAge() {
+		return studentAge;
+	}
+
+	public void setStudentAge(int studentAge) {
+		this.studentAge = studentAge;
+	}
+
+	@Override
+	public String toString() {
+		return "StudentDto [studentId=" + studentId + ", studentName=" + studentName + ", studentAge=" + studentAge
+				+ "]";
+	}
+
+}
